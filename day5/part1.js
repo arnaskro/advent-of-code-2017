6fdb12f6e7f547db0794593cf7fedf12f74fa5ef
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-12-05 06:30:10 +0100

    - Day 5 - Part 1

---
+++ day5/part1.js
@@ -0,0 +1,15 @@
+const parseData = () => document.body.innerText.split(" ").map(x => parseInt(x));
+const part1 = (input) => {
+  let steps = index = value = 0;
+
+  while (index < input.length) {
+    steps++;
+    value = input[index]
+    input[index] += 1
+    index += value
+  }
+
+  return steps;
+};
+
+console.log("Part 1 answer:", part1(parseData()));
